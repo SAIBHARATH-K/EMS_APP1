ea7991ca2a1ce64e2ab0be564b3c1e79e9070110
tarunperam05 <tarun.peram@gmail.com>
2026-05-06 14:54:08 +0530

    Updated code for PermanentEmployee.java

--- PermanentEmployee.java
+++ PermanentEmployee.java
@@ -2,12 +2,35 @@ package com.ibm.ems;
 
 public class PermanentEmployee extends Employee {
 
-    public PermanentEmployee(int id, String name, double salary) {
+    private double bonus;
+    private double taxRate = 0.05; // 5% tax
+
+    public PermanentEmployee(int id, String name, double salary, double bonus) {
         super(id, name, salary);
+        this.bonus = bonus;
     }
 
     @Override
     public double calculateSalary() {
-        return salary + (salary * 0.10);
+        double grossSalary = salary + (salary * 0.10) + bonus;
+        double tax = grossSalary * taxRate;
+        return grossSalary - tax;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void displayDetails() {
+        System.out.println("Permanent Employee Details:");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Base Salary: " + salary);
+        System.out.println("Bonus: " + bonus);
+        System.out.println("Net Salary: " + calculateSalary());
     }
 }
